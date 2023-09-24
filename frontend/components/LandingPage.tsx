fb4c112a8bef883a20c8b8fe435aee58ddfa1388
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-09-24 16:49:43 -0400

    Update LandingPage.tsx

--- frontend/components/LandingPage.tsx
+++ frontend/components/LandingPage.tsx
@@ -1,14 +1,10 @@
 // frontend/components/LandingPage.tsx
-import React, { FC, useRef, useEffect, useState } from 'react';
+
+import React, { FC, useRef } from 'react';
 import About from './About';  // Import the About component
 
 const LandingPage: FC = () => {
-  const [isClient, setIsClient] = useState(false);
   const aboutRef = useRef<HTMLDivElement>(null); // Create a ref to store the DOM element
-
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
 
   const handleContinue = () => {
     // Scroll down to the About section smoothly
@@ -20,25 +16,16 @@ const LandingPage: FC = () => {
 
   return (
     <>
-      {isClient && (
-        <StarfieldAnimation
-          style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-          }}
-        />
-      )}
       <main style={{ height: '100vh' }} className="relative flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-800 w-full">
-        {/* Landing Page Content */}
         
-        <div className="text-4xl font-semibold text-gray-800 dark:text-gray-100 mb-4 text-center">
+        {/* Landing Page Content */}
+        <div className="text-4xl font-semibold text-gray-800 dark:text-gray-100 mb-4 text-center z-10">
           <div>Nice to meet you,</div> 
           <div> I'm <span className="text-indigo-600 dark:text-indigo-400">Cameron Sivo</span>, a full-stack developer.</div>
         </div>
         <button 
           onClick={handleContinue} 
-          className="px-4 py-2 border-2 border-blue-500 text-blue-500 rounded hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-300 dark:border-blue-400 dark:text-blue-400 dark:hover:bg-blue-300 dark:hover:text-white dark:focus:ring-blue-200"
+          className="px-4 py-2 border-2 border-blue-500 text-blue-500 rounded hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-300 dark:border-blue-400 dark:text-blue-400 dark:hover:bg-blue-300 dark:hover:text-white dark:focus:ring-blue-200 z-10"
         >
           Continue to site
         </button>
