23af7a9c7e15a3c1044a49d112d9475f0aaf622f
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-09-24 16:00:11 -0400

    Update LandingPage.tsx

--- frontend/components/LandingPage.tsx
+++ frontend/components/LandingPage.tsx
@@ -1,7 +1,7 @@
 // frontend/components/LandingPage.tsx
 
-import React, { FC, useRef } from "react";
-import About from "./About"; // Import the About component
+import React, { FC, useRef } from 'react';
+import About from './About';  // Import the About component
 
 const LandingPage: FC = () => {
   const aboutRef = useRef<HTMLDivElement>(null); // Create a ref to store the DOM element
@@ -9,26 +9,22 @@ const LandingPage: FC = () => {
   const handleContinue = () => {
     // Scroll down to the About section smoothly
     if (aboutRef.current) {
-      aboutRef.current.scrollIntoView({ behavior: "smooth" });
+      aboutRef.current.scrollIntoView({ behavior: 'smooth' });
     }
     // Logic to show header can also be added here
   };
 
   return (
     <>
-      <main
-        style={{ height: "100vh" }}
-        className="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-800 w-full"
-      >
+      <main style={{ height: '100vh' }} className="flex flex-col items-center justify-center bg-gray-200 dark:bg-gray-800 w-full">
         {/* Landing Page Content */}
-        <div className="text-4xl font-bold text-gray-800 dark:text-gray-100 mb-4">
-          Hi! Nice to meet you,{" "}
-          <span className="font-extrabold">I'm Cameron Sivo</span>, a full-stack
-          developer.
+        <div className="text-4xl font-semibold text-gray-800 dark:text-gray-100 mb-4 text-center">
+          <div>Nice to meet you,</div> 
+          <div> I'm <span className="text-indigo-600 dark:text-indigo-400">Cameron Sivo</span>, a full-stack developer.</div>
         </div>
-        <button
-          onClick={handleContinue}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300 dark:bg-blue-400 dark:hover:bg-blue-300 dark:focus:ring-blue-200"
+        <button 
+          onClick={handleContinue} 
+          className="px-4 py-2 border-2 border-blue-500 text-blue-500 rounded hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-300 dark:border-blue-400 dark:text-blue-400 dark:hover:bg-blue-300 dark:hover:text-white dark:focus:ring-blue-200"
         >
           Continue to site
         </button>
