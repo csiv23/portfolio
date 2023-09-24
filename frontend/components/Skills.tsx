cc0ce402015b172784111dfeb5aa56ccd534051c
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-09-24 17:34:23 -0400

    Create Skills.tsx

---
+++ frontend/components/Skills.tsx
@@ -0,0 +1,32 @@
+// frontend/components/Skills.tsx
+import React, { FC } from "react";
+import { SiPython, SiJavascript, SiHtml5, SiCss3, SiGit, SiReact } from 'react-icons/si';
+import styles from './Skills.module.css';
+
+const Skills: FC = () => {
+  const skills = [
+    { name: "Python", icon: <SiPython /> },
+    { name: "JavaScript", icon: <SiJavascript /> },
+    { name: "HTML", icon: <SiHtml5 /> },
+    { name: "CSS", icon: <SiCss3 /> },
+    { name: "Git", icon: <SiGit /> },
+    { name: "React", icon: <SiReact /> },
+  ];
+
+  return (
+    <div style={{ backgroundColor: "rgb(26 27 27)" }} className="text-center p-6">
+      <h2 className="text-4xl font-semibold text-white">Skills & Technologies</h2>
+      <div className="flex flex-wrap gap-4 justify-center">
+        {skills.map((skill, index) => (
+          // Fallback to empty string if styles.skill-box is not defined
+<div key={index} className={`m-2 p-2 border rounded bg-gray-700 text-white flex flex-col items-center justify-center ${styles['skill-box'] || ''}`}>
+            <div className="text-4xl">{skill.icon}</div>
+            <div className="text-base">{skill.name}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
