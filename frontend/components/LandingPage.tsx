8c4661aa4490405f570ef769b796c9e998ec468b
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-10-23 19:54:22 -0400

    Fixed hovering removed dark mode

--- frontend/components/LandingPage.tsx
+++ frontend/components/LandingPage.tsx
@@ -1,44 +1,46 @@
 // frontend/components/LandingPage.tsx
 import React, { FC, RefObject } from "react";
-import './LandingPage.module.css'; // Import the CSS for hover animations
+import styles from "./LandingPage.module.css";
 
 interface LandingPageProps {
-  isDarkMode: boolean;
   aboutRef: RefObject<HTMLDivElement>;
 }
 
-const LandingPage: FC<LandingPageProps> = ({ isDarkMode, aboutRef }) => {
+const LandingPage: FC<LandingPageProps> = ({aboutRef }) => {
   const handleContinue = () => {
     if (aboutRef.current) {
       aboutRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
 
-  const bgColor = isDarkMode ? "black" : "black"; // Background color based on dark mode
 
   return (
     <main
       style={{
         height: "100vh",
-        backgroundColor: bgColor,
         position: "relative",
       }}
       className="flex flex-col items-center justify-center w-full"
     >
-      <div className="text-4xl font-semibold text-gray-800 dark:text-gray-100 mb-4 text-center">
-        <div>Nice to meet you,</div>
+      {/* Intro Lines */}
+      <div
+        style={{ fontSize: "48px", fontWeight: "bold" }}
+        className="text-4xl font-semibold text-gray-800 dark:text-gray-100 mb-4 text-center intro-line"
+      >
         <div>
-          I'm{" "}
-          <span className="text-indigo-600 dark:text-indigo-400">
-            Cameron Sivo
-          </span>
-          , a software engineer.
+          Hey, I'm{" "}
+          <span className="text-indigo-600 dark:text-indigo-400">Cameron</span>.
+          👋
         </div>
       </div>
-      <button
-        onClick={handleContinue}
-        className="custom-button"
+      <div
+        style={{ fontSize: "48px", fontWeight: "bold" }}
+        className="text-4xl font-semibold text-gray-800 dark:text-gray-100 mb-4 text-center intro-line"
       >
+        <div>I'm a Software Engineer.</div>
+      </div>
+
+      <button onClick={handleContinue} className="custom-button">
         Continue to site <span className="arrow right"></span>
       </button>
       <div className="absolute bottom-4 right-4 space-x-4">
@@ -54,7 +56,7 @@ const LandingPage: FC<LandingPageProps> = ({ isDarkMode, aboutRef }) => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <i className="fa fa-github fa-2x text-gray-800 hover:text-gray-700 dark:text-gray-100 dark:hover:text-gray-200"></i>
+          <i className="fa fa-github fa-2x text-gray-800 hover:text-gray-700 dark:text-gray-100 dark:hover:text-blue-500"></i>
         </a>
       </div>
     </main>
