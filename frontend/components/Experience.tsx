612eb5c24550ca75f98660a8b48335c102a26574
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-11-14 16:02:57 -0500

    Fixed coloring of hover effect

--- frontend/components/Experience.tsx
+++ frontend/components/Experience.tsx
@@ -1,6 +1,8 @@
-import React, { FC } from "react";
+import React, { FC, useContext } from "react";
 import useThemeStyles from "../hooks/useThemeStyles";
 import styles from "./Experience.module.css";
+import { ThemeContext } from "./ThemeContext";
+
 
 type Job = {
   company: string;
@@ -12,19 +14,21 @@ type Job = {
 
 const Experience: FC = () => {
   const { textClass } = useThemeStyles();
+  const darkTheme = useContext(ThemeContext);
+
   const jobs: Job[] = [
     {
       company: "Retail Business Services",
       role: "Data Engineer Co-op",
-      duration: "June 2023 - Present",
+      duration: "June - December 2023",
       description:
-        "Leveraging Databricks and Python to build and scale data pipelines, and automate data flows using PowerAutomate. Involved in the end-to-end data engineering process, including data sourcing, transformation, and loading (ETL).",
+        "Leveraged Databricks and Python to build and scale data pipelines, and automate data flows using PowerAutomate. Involved in the end-to-end data engineering process, including data sourcing, transformation, and loading (ETL).",
       technologies: ["Databricks", "Python", "PowerAutomate"],
     },
     {
       company: "Insulet Corporation",
       role: "Software Test Automation Co-op",
-      duration: "Jan 2022 - July 2022",
+      duration: "Janurary - July 2022",
       description:
         "Architected a Python-based automation framework to streamline the testing of OmniPod™ devices. Collaborated with a team of engineers to write rigorous test cases using pytest and unittest. Led a code refactoring initiative that resolved over 500 known issues, improving code maintainability.",
       technologies: ["Python", "pytest", "unittest"],
@@ -33,7 +37,15 @@ const Experience: FC = () => {
   return (
     <div
       id="experience"
-      style={{ paddingTop: "2vh" }}
+      style={{
+        paddingTop: "2vh",
+        margin: "0 auto",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        maxWidth: "1100px",
+        flexDirection: "column",
+      }}
       className="text-center w-full"
     >
       <h2 className={`text-4xl font-semibold mb-8 ${textClass}`}>Experience</h2>
@@ -41,15 +53,17 @@ const Experience: FC = () => {
         {jobs.map((job, index) => (
           <div
             key={index}
-            className={`col-span-4 grid grid-cols-4 w-full ${styles.jobBox}`}
+            className={`col-span-4 grid grid-cols-4 w-full ${styles.jobBox} ${
+              darkTheme ? styles.jobBoxDark : styles.jobBoxLight
+            }`}
           >
-            {/* Duration */}
             <div className={`col-span-1 text-lg ${textClass}`}>
               <p>{job.duration}</p>
             </div>
-            {/* Details */}
             <div className="col-span-3 text-left w-full">
-              <h3 className={`text-2xl font-semibold ${textClass}`}>{job.role}</h3>
+              <h3 className={`text-2xl font-semibold ${textClass}`}>
+                {job.role}
+              </h3>
               <h4 className={`text-xl ${textClass}`}>{job.company}</h4>
               <p className={`text-base ${textClass}`}>{job.description}</p>
               <div className="mt-2 flex space-x-2">
@@ -81,6 +95,5 @@ const Experience: FC = () => {
     </div>
   );
 };
-
 
 export default Experience;
