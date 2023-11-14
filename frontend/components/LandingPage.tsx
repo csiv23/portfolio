0c17a945ef9fef5a2f60b709c1016fe862640850
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-11-14 15:06:02 -0500

    Adjusted styling, added intro bio

--- frontend/components/LandingPage.tsx
+++ frontend/components/LandingPage.tsx
@@ -1,4 +1,3 @@
-// frontend/components/LandingPage.tsx
 import React, { FC, RefObject, useContext } from "react";
 import { ThemeContext, ThemeUpdateContext } from "./ThemeContext";
 import useThemeStyles from "../hooks/useThemeStyles";
@@ -23,38 +22,57 @@ const LandingPage: FC<LandingPageProps> = ({ aboutRef }) => {
       id="top"
       style={{
         height: "100vh",
-        position: "relative",
+        maxWidth: "700px",
+        width: "90%",
+        margin: "0 auto",
       }}
       className="flex flex-col items-center justify-center w-full"
     >
-      {/* Intro Lines */}
       <div
-        style={{ fontSize: "48px", fontWeight: "bold" }}
-        className={`text-4xl font-semibold mb-4 text-center intro-line ${textClass}`}
+        id="about"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
       >
-        <div>
+        <div
+          style={{ fontSize: "48px", fontWeight: "bold" }}
+          className={`text-4xl font-semibold mb-4 text-center intro-line ${textClass}`}
+        >
           Hey, I'm{" "}
           <span className={darkTheme ? styles.nameDark : styles.nameLight}>
             Cameron
           </span>
           . 👋
         </div>
-      </div>
-      <div
-        style={{ fontSize: "48px", fontWeight: "bold" }}
-        className={`text-4xl font-semibold mb-4 text-center intro-line ${textClass}`}
-      >
-        <div>I'm a Software Engineer.</div>
+        <div
+          style={{ fontSize: "48px", fontWeight: "bold" }}
+          className={`text-4xl font-semibold mb-4 text-center intro-line ${textClass}`}
+        >
+          <div>I'm a Software Engineer.</div>
+        </div>
+        <div id="about-description" style={{ paddingTop: "5px" }}>
+          <p className={`text-lg ${textClass}`}>
+            I'm a Computer Science graduate from Northeastern University with a
+            focus on full-stack development. Based in Boston, I'm deeply
+            committed to the craft of coding, from the logic of back-end to the
+            finesse of front-end design. My driving force is a strong work ethic
+            and the relentless pursuit of improvement. Let's build something
+            exceptional together.
+          </p>
+        </div>
+        <button
+          onClick={handleContinue}
+          className={`${styles.customButton} ${
+            darkTheme ? styles.customButtonDark : styles.customButtonLight
+          }`}
+        >
+          Continue to site <span className="arrow right"></span>
+        </button>
       </div>
 
-      <button
-        onClick={handleContinue}
-        className={`${styles.customButton} ${
-          darkTheme ? styles.customButtonDark : styles.customButtonLight
-        }`}
-      >
-        Continue to site <span className="arrow right"></span>
-      </button>
       <div className="absolute bottom-4 right-4 space-x-4">
         <a
           href="https://www.linkedin.com/in/cameron-sivo-68a721224/"
