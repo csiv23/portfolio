cc6d577efced7b5c13a1804e92bf8cd990400bd1
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-11-20 10:31:41 -0500

    Update About.tsx

--- frontend/components/About.tsx
+++ frontend/components/About.tsx
@@ -29,11 +29,35 @@ const About = forwardRef<HTMLDivElement>((props, ref) => {
           className="w-32 h-32 rounded-full mr-8"
         />
         <p className={`text-lg ${textClass}`}>
-          I'm a Computer Science graduate with a focus on full-stack
-          development. Based in Boston, I'm deeply committed to the craft of
-          coding, from the logic of back-end to the finesse of front-end design.
-          My driving force is a strong work ethic and the relentless pursuit of
-          improvement. Let's build something exceptional together.
+          <p>
+            My initial interest in software development began in high school
+            when I became the lead programmer of my robotics team after being
+            inspired by my older brother. This initial intrigue soon evolved
+            into a profound passion, leading me to major in Computer Science at
+            Northeastern University. At Northeastern, I've immersed myself in
+            various aspects of technology and cultivated my leadership skills.
+            More recently, I've found fulfillment in mentoring within the
+            Computer Science Mentorship Organization (CoSMO), where I guide
+            younger students towards achieving their goals and successes.
+          </p>
+          <p>
+            Outside of my technical pursuits, I am deeply involved in music,
+            leading several bands in Boston. I enjoy expressing myself through
+            the saxophone and guitar, exploring a range of genres from funk and
+            jazz to everything in-between. Additionally, I love to dedicate my
+            time to sailing—a passion influenced by my upbringing in Newport,
+            RI. My other interests include reading, playing fantasy football,
+            experimenting with ergonomic keyboards, and staying up to date on
+            the latest advancements in e-ink technology.
+          </p>
+          <p>
+            I firmly believe in the importance of a strong work ethic and
+            consistency. These principles are central to my approach to life,
+            propelling me to continually seek improvement and make a meaningful
+            impact in all my endeavors. At my core is a curiosity that drives me
+            to learn and grow, and I am always looking for new opportunities to
+            challenge myself.
+          </p>
         </p>
       </div>
     </div>
