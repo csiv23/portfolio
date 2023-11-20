431d9f60d36bb8f02c0569f661c50fdbf8fb812d
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-11-20 11:20:08 -0500

    Implemented contact form using Web3Forms

--- frontend/components/Contact.tsx
+++ frontend/components/Contact.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import useThemeStyles from "../hooks/useThemeStyles";
 import styles from "./Contact.module.css";
 
@@ -9,8 +9,8 @@ const Contact = () => {
     email: "",
     message: "",
   });
+  const [submissionStatus, setSubmissionStatus] = useState("");
 
-  // Handles changes to form fields
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
@@ -23,12 +23,38 @@ const Contact = () => {
   // Handles form submission
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("Form submitted:", formData);
+    try {
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          ...formData,
+          apikey: "07197a62-f470-448c-bde6-e7a47f4b8a3b",
+        }),
+      });
+
+      if (response.ok) {
+        setSubmissionStatus("Form submitted successfully");
+        setFormData({
+          name: "",
+          email: "",
+          message: "",
+        });
+      } else {
+        setSubmissionStatus("Submission failed. Please try again.");
+      }
+    } catch (error) {
+      console.error("Submission failed", error);
+      setSubmissionStatus("An error occurred. Please try again.");
+    }
   };
 
   return (
     <div id="contact" className={styles.contactContainer}>
-      <h2 className={`text-3xl font-bold mb-4  ${textClass}`}>Contact</h2>
+      <h2 className={`text-3xl font-bold mb-4 ${textClass}`}>Contact</h2>
       <p>Have a question or want to work together?</p>
       <form className={styles.contactForm} onSubmit={handleSubmit}>
         <input
@@ -61,6 +87,7 @@ const Contact = () => {
           Submit
         </button>
       </form>
+      {submissionStatus && <p>{submissionStatus}</p>}
     </div>
   );
 };
