679e6fec25dc4256ea4762cbcffa90f1d5a936b7
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-11-21 10:52:22 -0500

    Implemented smooth scrolling

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,13 +1,26 @@
-// frontend/components/Navbar.tsx
 import React, { FC, useContext } from "react";
 import { ThemeContext, ThemeUpdateContext } from "./ThemeContext";
 import MoonIcon from "@mui/icons-material/DarkModeOutlined";
 import SunIcon from "@mui/icons-material/LightModeOutlined";
-import styles from "./Navbar.module.css"; // Your CSS module for styling
+import styles from "./Navbar.module.css";
 
 const Navbar: FC = () => {
   const darkTheme = useContext(ThemeContext);
   const toggleTheme = useContext(ThemeUpdateContext);
+
+  const scrollToSection = (sectionId: string, offset: number) => {
+    return (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      event.preventDefault();
+      const section = document.getElementById(sectionId);
+      if (section) {
+        const offsetTop = section.offsetTop + offset; // Use the offset here
+        window.scrollTo({
+          top: offsetTop,
+          behavior: "smooth",
+        });
+      }
+    };
+  };
 
   return (
     <nav
@@ -16,12 +29,22 @@ const Navbar: FC = () => {
       }`}
     >
       <div className={styles.navLinks}>
-        <a href="#top">Cameron Sivo</a>
-        <a href="#about">About Me</a>
-        <a href="#skills">Skills</a>
-        <a href="#projects">Projects</a>
-        <a href="#experience">Experience</a>
-        <a href="#contact">Contact</a>
+        <a href="#top"  onClick={scrollToSection("top", -100)}>Cameron Sivo</a>
+        <a href="#about" onClick={scrollToSection("about", 400)}>
+          About Me
+        </a>
+        <a href="#skills" onClick={scrollToSection("skills", -55)}>
+          Skills
+        </a>
+        <a href="#experience" onClick={scrollToSection("experience", -55)}>
+          Experience
+        </a>
+        <a href="#projects" onClick={scrollToSection("projects", -55)}>
+          Projects
+        </a>
+        <a href="#contact" onClick={scrollToSection("contact", -55)}>
+          Contact
+        </a>
       </div>
       <button
         onClick={toggleTheme}
