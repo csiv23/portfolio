3a3b93d383f3a8a375783fe403ab3098649105cd
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-11-21 16:28:36 -0500

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,16 @@
-import Head from 'next/head';
-import { FC } from 'react';
-import Main from '../components/Main';
+import Head from "next/head";
+import { FC } from "react";
+import Main from "../components/Main";
 
 const Home: FC = () => {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="Cameron Sivo Portfolio" />
+        <meta name="keywords" content="Cameron, Sivo, Portfolio" />
+        <meta name="author" content="Cameron Sivo" />
         <title>Cameron Sivo</title>
       </Head>
       <Main />
