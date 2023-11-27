2613db196af8bb2ac9bf7efe9033aae9fe5b6a69
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2023-11-27 14:17:37 -0500

    Fixed scrolling behavior

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,75 +1,49 @@
-import React, { FC, useContext, useState } from "react";
-import { ThemeContext, ThemeUpdateContext } from "./ThemeContext";
-import MoonIcon from "@mui/icons-material/DarkModeOutlined";
-import SunIcon from "@mui/icons-material/LightModeOutlined";
-import styles from "./Navbar.module.css";
+import React, { FC, useContext, useState } from 'react';
+import { ThemeContext, ThemeUpdateContext } from './ThemeContext';
+import MoonIcon from '@mui/icons-material/DarkModeOutlined';
+import SunIcon from '@mui/icons-material/LightModeOutlined';
+import MenuIcon from '@mui/icons-material/Menu'; 
+import styles from './Navbar.module.css';
 
 const Navbar: FC = () => {
   const darkTheme = useContext(ThemeContext);
   const toggleTheme = useContext(ThemeUpdateContext);
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [navOpen, setNavOpen] = useState(false);
 
-  const scrollToSection = (sectionId: string) => {
-    return () => {
+  const scrollToSection = (sectionId: string, offset: number) => {
+    return (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      event.preventDefault();
+      setNavOpen(false); // Close the nav menu when a link is clicked
       const section = document.getElementById(sectionId);
       if (section) {
+        const offsetTop = section.offsetTop + offset;
         window.scrollTo({
-          top: section.offsetTop,
-          behavior: "smooth",
+          top: offsetTop,
+          behavior: 'smooth',
         });
-        setIsNavOpen(false); // Close the menu after clicking a link
       }
     };
   };
 
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
-  };
-
   return (
-    <nav
-      className={`${styles.navbar} ${
-        darkTheme ? styles.navbarDark : styles.navbarLight
-      }`}
-    >
-      <button className={styles.navbarToggle} onClick={toggleNav}>
-        {isNavOpen ? "×" : "☰"}
+    <nav className={`${styles.navbar} ${darkTheme ? styles.navbarDark : styles.navbarLight}`}>
+      <button className={styles.navbarToggle} onClick={() => setNavOpen(!navOpen)}>
+        <MenuIcon /> 
       </button>
-      <div
-        className={`${styles.navLinks} ${isNavOpen ? styles.navOpen : ""} ${
-          darkTheme ? styles.navOpenDark : styles.navOpenLight
-        }`}
-      >
-        <a
-          className={darkTheme ? styles.nameDark : styles.nameLight}
-          href="#top"
-          onClick={scrollToSection("top")}
-        >
-          Cameron Sivo
-        </a>
 
-        <a href="#about" onClick={scrollToSection("about")}>
-          About Me
-        </a>
-        <a href="#skills" onClick={scrollToSection("skills")}>
-          Skills
-        </a>
-        <a href="#experience" onClick={scrollToSection("experience")}>
-          Experience
-        </a>
-        <a href="#projects" onClick={scrollToSection("projects")}>
-          Projects
-        </a>
-        <a href="#contact" onClick={scrollToSection("contact")}>
-          Contact
-        </a>
+      <div className={`${styles.navLinks} ${navOpen ? styles.navOpen : ''}`}>
+        <a href="#top" onClick={scrollToSection('top', -100)}>Cameron Sivo</a>
+        <a href="#about" onClick={scrollToSection('about', -100)}>About Me</a>
+        <a href="#skills" onClick={scrollToSection('skills', -55)}>Skills</a>
+        <a href="#experience" onClick={scrollToSection('experience', -55)}>Experience</a>
+        <a href="#projects" onClick={scrollToSection('projects', -55)}>Projects</a>
+        <a href="#contact" onClick={scrollToSection('contact', -55)}>Contact</a>
       </div>
+
       <button
         onClick={toggleTheme}
         className={`${styles.themeToggleButton} ${
-          darkTheme
-            ? styles.themeToggleButtonDark
-            : styles.themeToggleButtonLight
+          darkTheme ? styles.themeToggleButtonDark : styles.themeToggleButtonLight
         }`}
       >
         {darkTheme ? <SunIcon /> : <MoonIcon />}
